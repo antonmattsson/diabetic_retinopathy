06a12f657afb81e00c7205034c72990de6aca08d
Anton Mattsson <anton.matinpoika@gmail.com>
2018-11-27 20:33:36 +0200

    Data augmentation

---
+++ src/CNN_augmented.py
@@ -0,0 +1,79 @@
+import numpy as np
+from keras.layers import Flatten, Dense, ZeroPadding2D, Conv2D, Activation, MaxPooling2D, Dropout
+from keras.preprocessing.image import ImageDataGenerator
+from keras.layers import Flatten, Dense, Conv2D, Activation, MaxPooling2D, Dropout
+from keras.models import Sequential
+from keras.callbacks import EarlyStopping
+import pickle
+
+# Set the number of training samples
+n_total = 2400
+batch_size = 32
+img_shape = (256, 256)
+
+# Set data augmentation protocol
+datagen = ImageDataGenerator(
+        rotation_range=40,
+        width_shift_range=0.2,
+        height_shift_range=0.2,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True,
+        fill_mode='nearest')
+
+# Construct generators for the training and validation data using data augmentation
+train_gen = datagen.flow_from_directory('../data/augmentation_train',
+                                        target_size=(256, 256),
+                                        batch_size=batch_size)
+
+test_gen = datagen.flow_from_directory('../data/augmentation_test',
+                                        target_size=(256, 256),
+                                        batch_size=batch_size)
+
+#add convolutional network model
+
+model = Sequential()
+model.add(Conv2D(64, kernel_size=(4, 4), input_shape=(img_shape[0], img_shape[1], 3)))
+model.add(Activation('relu'))
+
+
+model.add(Conv2D(64, kernel_size=(4, 4), padding='same'))
+model.add(Activation('relu'))
+
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(64, kernel_size=(4, 4), padding='same'))
+model.add(Activation('relu'))
+
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Flatten())
+model.add(Activation('relu'))
+
+model.add(Dense(64, activation="relu"))
+
+model.add(Dropout(0.5))
+model.add(Dense(5, activation="softmax"))
+
+#compile and fit model
+
+earlystop = EarlyStopping(monitor='val_loss', min_delta=0.0001, patience=15, \
+                          verbose=0, mode='auto')
+callbacks_list = [earlystop]
+
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+n_epochs = 20
+history_dict = None
+# Save model and history dict between every epoch
+for i in range(n_epochs):
+    print("\nEpoch " + str(i+1) + "/" + str(n_epochs))
+    history = model.fit_generator(generator=train_gen, validation_data=test_gen,
+                        steps_per_epoch=50,
+                        epochs=1, verbose=2, callbacks=callbacks_list)
+    history_dict = add_to_history(history_dict, history)
+    model.save('model_downsampled.h5')
+    with open('history_downsampled', 'wb') as file_pi:
+        pickle.dump(history_dict, file_pi)
+
+
+
---
+++ src/data_for_augmentation.py
@@ -0,0 +1,61 @@
+import numpy as np
+from skimage.io import imread, imsave
+from skimage import color, exposure
+from numpy.core.defchararray import add, replace
+
+def change_exposure(fname, label, folder):
+    img = imread('../data/converted/'+ fname + '.jpeg')
+    hsv = color.rgb2hsv(img)
+    hsv[:, :, 2] = exposure.equalize_hist(hsv[:, :, 2])
+    img = color.hsv2rgb(hsv)
+    img = img / 255
+    imsave(folder + label + '/' + fname + '.png', img, plugin='png')
+
+full_filenames = np.genfromtxt('../data/train_filenames.txt', dtype=str)
+n_total = len(full_filenames)
+# Set the number of training samples
+n_train = int(np.ceil(n_total * 0.8))
+n_test = int(np.floor(n_total * 0.2))
+
+# Read the labels file
+full_labels = np.genfromtxt('../data/trainLabels.csv', skip_header=1, dtype=str, delimiter=',')
+# Keep only labels of data that can be used in training
+full_samples = replace(full_filenames, ".jpeg", "")
+full_mask = np.isin(full_labels[:, 0], full_samples)
+trainable_labels = np.copy(full_labels[full_mask, :])
+
+# Downsample the zero grade, keeping only the first 5000
+# Randomize order
+np.random.seed(1234)
+np.random.shuffle(trainable_labels)
+# Arrange by a stable sort (mergesort)
+trainable_labels = np.copy(trainable_labels[trainable_labels[:,1].argsort(kind='mergesort')])
+# Remove extra zeros
+zeros_left = 5000
+if zeros_left > 0:
+    _, counts = np.unique(trainable_labels[:,1], return_counts=True)
+    n_zeros = counts[0]
+    downsampled_labels = np.copy(trainable_labels[(n_zeros-zeros_left):, :])
+else:
+    downsampled_labels = np.copy(trainable_labels)
+
+
+np.random.shuffle(downsampled_labels)
+train_labels = downsampled_labels[:n_train, :]
+#test_labels = downsampled_labels[n_train:(n_train + n_test)]
+# Exclude training samples from the original data and choose test data among them
+np.random.shuffle(trainable_labels)
+exclusion = np.isin(trainable_labels[:, 0], train_labels[:, 0], invert=True)
+valid_labels = np.copy(trainable_labels[exclusion, :])
+test_labels = np.copy(valid_labels[:n_test, :])
+
+# Save images into subfolders by class
+for i in range(n_train):
+    change_exposure(fname=downsampled_labels[i, 0],
+                    label=downsampled_labels[i, 1],
+                    folder='../data/augmentation_train/')
+
+for j in range(n_test):
+    change_exposure(fname=downsampled_labels[i, 0],
+                    label=downsampled_labels[i, 1],
+                    folder='../data/augmentation_validation/')
