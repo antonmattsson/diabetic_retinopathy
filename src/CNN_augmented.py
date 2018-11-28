9442041cfc8a2df7bd274cc9500c7e76701f70f3
Anton Mattsson <anton.matinpoika@gmail.com>
2018-11-28 21:43:50 +0200

    Loading pretrained model

--- src/CNN_augmented.py
+++ src/CNN_augmented.py
@@ -2,13 +2,14 @@ import numpy as np
 from keras.layers import Flatten, Dense, ZeroPadding2D, Conv2D, Activation, MaxPooling2D, Dropout
 from keras.preprocessing.image import ImageDataGenerator
 from keras.layers import Flatten, Dense, Conv2D, Activation, MaxPooling2D, Dropout
-from keras.models import Sequential
+from keras.models import Sequential, load_model
 from keras.callbacks import EarlyStopping
 import pickle
 from performance_plots import *
+from pathlib import Path
 
 # Set the number of training samples
-train_steps = 300
+train_steps = 400
 validation_steps = 75 # maximum
 batch_size = 32
 img_shape = (256, 256)
@@ -27,33 +28,44 @@ train_gen = datagen.flow_from_directory('../data/augmentation_train',
                                         target_size=(256, 256),
                                         batch_size=batch_size)
 
-test_gen = datagen.flow_from_directory('../data/augmentation_validation',
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+test_gen = test_datagen.flow_from_directory('../data/augmentation_validation',
                                         target_size=(256, 256),
                                         batch_size=batch_size)
 
-#add convolutional network model
+# Check if a pretrained model exists
+model_path = "model_augmented.h5"
+model_file = Path(model_path)
+if model_file.is_file():
+    model = load_model(model_path)
+else: # Start from scratch
 
-model = Sequential()
-model.add(Conv2D(64, kernel_size=(4, 4), input_shape=(img_shape[0], img_shape[1], 3)))
-model.add(Activation('relu'))
+    #add convolutional network model
+
+    model = Sequential()
+    model.add(Conv2D(64, kernel_size=(4, 4), input_shape=(img_shape[0], img_shape[1], 3)))
+    model.add(Activation('relu'))
 
 
-model.add(Conv2D(64, kernel_size=(4, 4), padding='same'))
-model.add(Activation('relu'))
+    model.add(Conv2D(64, kernel_size=(4, 4), padding='same'))
+    model.add(Activation('relu'))
 
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Conv2D(64, kernel_size=(4, 4), padding='same'))
-model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Conv2D(64, kernel_size=(4, 4), padding='same'))
+    model.add(Activation('relu'))
 
-model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
 
-model.add(Flatten())
-model.add(Activation('relu'))
+    model.add(Flatten())
+    model.add(Activation('relu'))
 
-model.add(Dense(64, activation="relu"))
+    model.add(Dense(64, activation="relu"))
 
-model.add(Dropout(0.5))
-model.add(Dense(5, activation="softmax"))
+    model.add(Dropout(0.5))
+    model.add(Dense(5, activation="softmax"))
+
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
 #compile and fit model
 
@@ -61,9 +73,7 @@ earlystop = EarlyStopping(monitor='val_loss', min_delta=0.0001, patience=15, \
                           verbose=0, mode='auto')
 callbacks_list = [earlystop]
 
-model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-
-n_epochs = 20
+n_epochs = 50
 history_dict = None
 # Save model and history dict between every epoch
 for i in range(n_epochs):
